db414e52d2641a068b70326fd657cb06462c2d7a
DevCodeJMMarilyn <jimenezm.m2021@gmail.com>
2024-08-30 13:39:17 -0600

    Subiendo ejercicio

---
+++ ejercicio.py
@@ -0,0 +1,11 @@
+# Creacion de vector
+nombres= ['Juan', 'Manuel', 'Sofia']
+#Imprimir vector
+#print (nombres[1], "-", nombres [2])
+
+Notas= [8,4,8,10,3]
+#print (Notas[4])
+#print("El promedio del estudiante es: ", sum (Notas)/len (Notas))
+
+Char=['D', 'S',"e"]
+print (Char)
