fe94e9483e323174b1b6190ea73519f3d52cb0c2
StefanyGHA <139899073+StefanyGHA@users.noreply.github.com>
2024-08-30 16:01:27 -0600

    Add files via upload

---
+++ ED-Ejercicios/ejercicios.py
@@ -0,0 +1,12 @@
+#Creacion de Vectores
+nombres=['Juan', 'Manuel', 'Sofia']
+#Imprimir Vector
+print(nombres[1], "-",nombres[2])
+
+#Notas
+Notas=[8,4,8,9,10]
+print(Notas[4])
+print("El promedio del estudiante es: ", sum(Notas)/len(Notas))
+
+Char=["S", "T", "E"]
+print(Char)
