45d5f8e7c331312145a5264b0b247906876a3ca8
oscar08k <159199501+oscar08k@users.noreply.github.com>
2024-09-04 20:35:26 -0600

    Add files via upload

---
+++ Ejercicios-tarea/ejercicio1.py
@@ -0,0 +1,11 @@
+nombres=['juan', 'manuel', 'sofia']
+print(nombres[1],"-",nombres[2])
+
+
+notas=[8,4,8,10,3]
+print(notas[4])
+
+print("el promedio del estudiante es ", sum(notas)/len(notas))
+
+Char=['D','S','O', 2,2,2,2,"e"]
+print(Char [2])
